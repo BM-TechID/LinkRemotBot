23c7fa76a6624ee2fb272a0bc48dffa1d4c0f785
BM-TechID <50511924+BM-TechID@users.noreply.github.com>
2021-12-17 10:16:39 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,7 @@
 rm -rf logremot.txt
 screen -dmS remot -L -Logfile /logremot.txt ssh - .ssh/remot -o StrictHostKeyChecking=no -R 80:localhost:80 localhost.run
 ./bot
+clear
+echo "Sudah dikirim ke Telegramu"
 host=$(cat /logremot.txt|grep tunneled|awk '{print $1}')
 echo "Host: $host"
