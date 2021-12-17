a14dc0d48df7c51cc842b4a6323851df10a04a6c
BM-TechID <50511924+BM-TechID@users.noreply.github.com>
2021-12-17 11:36:11 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,8 @@ sleep 2; echo "Run script..."
 ./run
 sleep 2; echo "Send to bot..."
 ./bot
+clear
 sleep 2; clear; echo " Selesai"
 echo ""
-host=$(cat /logremot.txt|grep tunneled|awk '{print $1}')
+host=$(cat logremot.txt|grep tunneled|awk '{print $1}')
 echo "Remot Host: $host"
